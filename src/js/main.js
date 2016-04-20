9ce872493862cc4081a82ea34e6c0eef7dc2348e
khalilgharbaoui <kaygeee@gmail.com>
2016-04-20 11:22:29 +0200

    added the mainjs and tested the boilerplate

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import App from './components/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppAPI from './utils/appAPI';
+
+ReactDOM.render((
+<App />
+), document.getElementById('app'));
